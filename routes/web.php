39443b46b2cd3622c62f4792700c43f61cf6a991
Adi <adisetiono1911@gmail.com>
2025-07-25 21:35:26 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/dashboard', function () {
-    return view('dashboard.index');
-})->name('dashboard');
-
-Route::get('/analytics', function () {
-    return view('dashboard.analytics');
-})->name('analytics');
-
-Route::get('/users', function () {
-    return view('dashboard.users');
-})->name('users');
-
-Route::get('/settings', function () {
-    return view('dashboard.settings');
-})->name('settings');
+Route::get('/', function () {
+    return view('welcome');
+});
